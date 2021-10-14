c2d45d1d8de1d933e63130cc06c474f6b1f68796
jason-nn <jason.ho@obf.ateneo.edu>
2021-10-14 17:58:02 +0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./Components/Shared/Navbar";
+import Home from "./Components/Home/Home";
+import Summary from "./Components/Summary/Summary";
 
 export default function App() {
+    const [DisplayHome, setDisplayHome] = useState(true);
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <>
+            <Navbar setDisplayHome={(i) => setDisplayHome(i)} />
+            {DisplayHome ? <Home /> : <Summary />}
+        </>
     );
 }
---
+++ src/Components/Shared/Navbar.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+export default function Navbar({ setDisplayHome }) {
+    const [Time, setTime] = useState(
+        new Date().toLocaleTimeString([], {
+            hour: "numeric",
+            minute: "numeric",
+        })
+    );
+
+    useEffect(() => {
+        let TimeInterval = setInterval(() => {
+            setTime(
+                new Date().toLocaleTimeString([], {
+                    hour: "numeric",
+                    minute: "numeric",
+                })
+            );
+        }, 1000);
+        return () => {
+            clearInterval(TimeInterval);
+        };
+    }, []);
+
+    return (
+        <div className="Navbar">
+            <div className="Left">
+                <span>{Time}</span>
+            </div>
+            <div className="Right">
+                <span onClick={() => setDisplayHome(true)}>Home</span>
+                <span onClick={() => setDisplayHome(false)}>Summary</span>
+            </div>
+        </div>
+    );
+}
