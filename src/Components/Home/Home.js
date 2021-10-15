089a9e10381bcf233ceae56a286aca1fa740205a
jason-nn <jason.ho@obf.ateneo.edu>
2021-10-15 17:28:21 +0800

    styled form

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,67 +12,77 @@ export default function Home({
 
     return (
         <div className="Home">
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    const hours = parseFloat(hoursRef.current.value);
-                    const tag = tagRef.current.value;
-                    const activity = activityRef.current.value;
+            <div className="Left"></div>
+            <div className="Right">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        const hours = parseFloat(hoursRef.current.value);
+                        const tag = tagRef.current.value;
+                        const activity = activityRef.current.value;
 
-                    if (hours && tag && activity) {
-                        if (hours > 0) {
-                            const date = new Date().toLocaleDateString();
+                        if (hours && tag && activity) {
+                            if (hours > 0) {
+                                const date = new Date().toLocaleDateString();
 
-                            setAllCheckIns([
-                                ...AllCheckIns,
-                                { date, hours, tag, activity },
-                            ]);
+                                setAllCheckIns([
+                                    ...AllCheckIns,
+                                    { date, hours, tag, activity },
+                                ]);
 
-                            localStorage.AllCheckIns = JSON.stringify([
-                                ...AllCheckIns,
-                                { date, hours, tag, activity },
-                            ]);
+                                localStorage.AllCheckIns = JSON.stringify([
+                                    ...AllCheckIns,
+                                    { date, hours, tag, activity },
+                                ]);
 
-                            hoursRef.current.value = null;
-                            tagRef.current.value = null;
-                            activityRef.current.value = null;
-                            setError(null);
-                            setSuccess("Successfully added check in");
-                            setTimeout(() => {
+                                hoursRef.current.value = null;
+                                tagRef.current.value = null;
+                                activityRef.current.value = null;
+                                setError(null);
+                                setSuccess("Successfully added check in");
+                                setTimeout(() => {
+                                    setSuccess(null);
+                                }, 2000);
+                            } else {
                                 setSuccess(null);
-                            }, 2000);
+                                setError(
+                                    "Please make sure Hours is a positive number"
+                                );
+                                setTimeout(() => {
+                                    setError(null);
+                                }, 2000);
+                            }
                         } else {
                             setSuccess(null);
                             setError(
-                                "Please make sure Hours is a positive number"
+                                "Please make sure all fields are filled in"
                             );
                             setTimeout(() => {
                                 setError(null);
                             }, 2000);
                         }
-                    } else {
-                        setSuccess(null);
-                        setError("Please make sure all fields are filled in");
-                        setTimeout(() => {
-                            setError(null);
-                        }, 2000);
-                    }
-                }}
-            >
-                <label>
-                    Hours
-                    <input type="number" step="any" ref={hoursRef} />
-                </label>
-                <label>
-                    Tag
-                    <input type="text" ref={tagRef} />
-                </label>
-                <label>
-                    Activity
-                    <input type="text" ref={activityRef} />
-                </label>
-                <button>Add</button>
-            </form>
+                    }}
+                >
+                    <h2>Add a check in</h2>
+                    <div>
+                        <label>
+                            <div>Hours</div>
+                            <input type="number" step="any" ref={hoursRef} />
+                        </label>
+                        <label>
+                            <div>Tag</div>
+                            <input type="text" ref={tagRef} />
+                        </label>
+                        <label>
+                            <div>Activity</div>
+                            <input type="text" ref={activityRef} />
+                        </label>
+                    </div>
+                    <div>
+                        <button>Add</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
